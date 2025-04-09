7b609a93df2a7615715b7b97a45cbfa13d999294
mgah77 <mauricio.gah@gmail.com>
2025-04-09 15:51:23 -0400

    456

--- models/prestamo.py
+++ models/prestamo.py
@@ -7,13 +7,13 @@ class Prestamo(models.Model):
 
     name = fields.Integer(string='index', index=True)
     nombre = fields.Many2one('hr.employee',string='Empleado', required=True) 
-    monto = fields.Integer(string='Prestamo', required=True)
+    monto = fields.Integer(string='Monto del Prestamo', required=True)
     nro_cuotas = fields.Integer(string='Nro Cuotas', required=True)
     cuota = fields.Integer(string='Cuota')
     restante = fields.Integer(string='Cuotas Restantes')
-    saldo  = fields.Integer(string='Saldo')
+    saldo  = fields.Integer(string='Saldo Restante')
     fecha = fields.Date(string='Fecha de Solicitud')
-    activo = fields.Boolean(string='activo', default = True)
+    activo = fields.Boolean(string='activo', default = False)
 
     @api.constrains('nro_cuotas')
     def _check_cuota(self):
