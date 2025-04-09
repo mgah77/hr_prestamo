b00f35c601fa40d71be4274a7278330f7a46938b
mgah77 <mauricio.gah@gmail.com>
2025-04-09 15:42:28 -0400

    pres

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{ 'name': 'HR Prestamos',
+'summary': "Prestamos",
+'author': "Mauricio Gah",
+'license': "AGPL-3",
+'application': "True",
+'version': "2.0",
+'data': ['security/groups.xml',
+         'security/ir.model.access.csv',     
+         'views/prestamo.xml',
+        
+],
+
+'depends': ['base' , 'contacts' , 'hr' , 'parches'],
+'installable': True,
+'application': True,
+}
---
+++ models/prestamo.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class Prestamo(models.Model):
+    _name = 'hr.prestamo'
+    _description = 'Prestamos a empleados'
+
+    name = fields.Integer(string='index', index=True)
+    nombre = fields.Many2one('hr.employee',string='Empleado', required=True) 
+    monto = fields.Integer(string='Prestamo', required=True)
+    nro_cuotas = fields.Integer(string='Nro Cuotas', required=True)
+    cuota = fields.Integer(string='Cuota')
+    restante = fields.Integer(string='Cuotas Restantes')
+    saldo  = fields.Integer(string='Saldo')
+    fecha = fields.Date(string='Fecha de Solicitud')
+    activo = fields.Boolean(string='activo', default = True)
+
+    @api.constrains('nro_cuotas')
+    def _check_cuota(self):
+        for record in self:
+            if record.cuotas and record.matricula <= 0:
+                raise ValidationError("El número de cuotas no puede ser 0 o menor.")
+
